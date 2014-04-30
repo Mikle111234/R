97c4302558e9deb076321d2d2566058106300453
Mikle111234 <ulianin_m@mail.ru>
2014-04-30 05:05:38 +0300

    delete figure

--- R/Program.cs
+++ R/Program.cs
@@ -5,44 +5,6 @@ using System.Text;
 
 namespace ConsoleApplication1
 {
-    abstract class Figure
-    {
-        //Площадь фигуры 
-        public abstract double square();
-        public abstract double perimeter();
-    }
-
-    class Triangle : Figure
-    {
-        double a, b, c; //Стороны 
-        //Конструктор 
-        public Triangle(double a, double b, double c)
-        {
-            this.a = a;
-            this.b = b;
-            this.c = c;
-        }
-        public override double square()
-        {
-            //Используем формулу Герока 
-            double p = (a + b + c) / 2;
-            return Math.Sqrt(p * (p - a) * (p - b) * (p - c));
-        }
-        public override double perimeter()
-        {
-            double per = a + b + c;
-            return per;
-        }
-    }
-
-    class PrintFigur
-    {
-        public void print(Figure f)
-        {
-            Console.WriteLine("perimetr figure = " + f.perimeter() + ", square figure = " + f.square());
-        }
-    }
-
     class Massive
     {
         public int Sum(int[] x)
