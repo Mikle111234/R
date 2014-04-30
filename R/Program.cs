bb83fde1821fe74207ea3d2be12e3caa96510e78
Mikle111234 <ulianin_m@mail.ru>
2014-04-30 04:38:22 +0300

    class massive

--- R/Program.cs
+++ R/Program.cs
@@ -5,6 +5,44 @@ using System.Text;
 
 namespace ConsoleApplication1
 {
+    class Massive
+    {
+        public int Sum(int[] x)
+        {
+            int summa = 0;
+            foreach (int c in x)
+            { summa += c; }
+            return summa;
+        }
+        public int Sum1(int[] x)
+        {
+            int summa = 0;
+            bool f = false;
+            foreach (int c in x)
+            {
+                if (f)
+                {
+                    summa += c;
+                    f = false;
+                }
+                else
+                    f = true;
+            }
+            return summa;
+        }
+        public int SumOtr(int[] x)
+        {
+            int summa = 0;
+            foreach (int c in x)
+            {
+                if (c < 0)
+                    return summa;
+                summa += c;
+            }
+            return summa;
+        }
+    }
+
     class Program
     {
         static void Main(string[] args)
